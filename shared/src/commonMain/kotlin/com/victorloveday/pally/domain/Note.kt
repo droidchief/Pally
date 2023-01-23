175e3b1d9907a91afbf769473c1317009f871820
Vicviral <lovedayv7@gmail.com>
2023-01-23 23:21:55 +0100

    Feat: create note data class and color file to generate color at rnadom

---
+++ shared/src/commonMain/kotlin/com/victorloveday/pally/domain/Note.kt
@@ -0,0 +1,20 @@
+package com.victorloveday.pally.domain
+
+import com.victorloveday.pally.presentation.*
+import kotlinx.datetime.LocalDateTime
+
+/**
+ *Created by Victor Loveday on 1/23/23
+ */
+data class Note(
+    val id: Long?,
+    val title: String,
+    val content: String,
+    val contentColor: Long,
+    val dateCreated: LocalDateTime
+) {
+    companion object {
+        private val colors = listOf(RedOrangeHex, RedPinkHex, BabyBlueHex, VioletHex, LightGreenHex)
+        fun generateRandomColour() = colors.random()
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/victorloveday/pally/presentation/Colors.kt
@@ -0,0 +1,10 @@
+package com.victorloveday.pally.presentation
+
+/**
+ *Created by Victor Loveday on 1/23/23
+ */
+const val RedOrangeHex = 0xfffab91
+const val RedPinkHex = 0xfff48fb1
+const val BabyBlueHex = 0xff81deea
+const val VioletHex = 0xffcf94da
+const val LightGreenHex = 0xffe7ed9b
